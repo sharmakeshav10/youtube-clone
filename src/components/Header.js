b15fdde0dea5d7dc488a1cbce5eff4fdedd16036
Keshav Sharma <sharmakeshav0708@gmail.com>
2024-05-07 01:52:54 +0530

    implemented search functionality with debouncing and autocomplete

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,30 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/slices/appSlice";
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/config/app_config";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+  const [showSearchSuggestions, setShowSearchSuggestions] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    console.log("API Call - " + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSearchSuggestions(json[1]);
+    console.log(json);
+  };
 
   const sideBarMenuToggleHandler = () => {
     dispatch(toggleMenu());
@@ -26,15 +47,39 @@ const Header = () => {
           src="https://t3.ftcdn.net/jpg/05/07/46/84/360_F_507468479_HfrpT7CIoYTBZSGRQi7RcWgo98wo3vb7.jpg"
         />
       </div>
-      <div className="col-span-8 flex justify-center">
-        <input
-          className="w-1/2 border border-gray-400 rounded-full rounded-r-none p-2"
-          placeholder="Search"
-          type="type"
-        />
-        <button className="border border-gray-400 bg-gray-500 p-2 px-6 rounded-full rounded-l-none text-white">
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
+
+      <div className="col-span-8">
+        <div className="flex justify-center">
+          <input
+            className="w-1/2 border border-gray-400 rounded-full rounded-r-none p-2"
+            placeholder="Search"
+            type="type"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSearchSuggestions(true)}
+            onBlur={() => setShowSearchSuggestions(false)}
+          />
+          <button className="border border-gray-400 bg-gray-500 p-2 px-6 rounded-full rounded-l-none text-white">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className="px-56 py-1">
+          {showSearchSuggestions && (
+            <div className="fixed bg-white w-[32rem] py-2 shadow-lg border border-gray-200 rounded-lg">
+              <ul>
+                {searchSuggestions.map((suggestion) => (
+                  <li className="py-1 hover:bg-gray-100 px-6 cursor-pointer">
+                    <span className="mr-3">
+                      <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </span>
+                    {suggestion}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
       <div className="col-span-2 flex justify-end">
         <FontAwesomeIcon icon={faUser} />
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,13 +1,32 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeSidebarMenu } from "../utils/slices/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  const videoId = searchParams.get("v");
+
   useEffect(() => {
     dispatch(closeSidebarMenu());
   }, []);
-  return <div>WatchPage</div>;
+  return (
+    <div className="w-full m-4">
+      <iframe
+        className="w-2/3 rounded-lg"
+        height="550"
+        src={"https://www.youtube.com/embed/" + videoId}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
